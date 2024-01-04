870ccd8daa4783d996ada189ef680ca0a7a4eb4a
Yemata Ferry <yemataferry@gmail.com>
2024-01-04 09:31:18 +0100

    linter errors arranged

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,12 @@ const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
-let activeSlide = 0;
+let activeSlide = 0
 
 rightBtn.addEventListener('click', () => {
   activeSlide++
 
-  if (activeSlide > slides.length -1) {
+  if (activeSlide > slides.length - 1) {
     activeSlide = 0
   }
 
@@ -20,7 +20,7 @@ leftBtn.addEventListener('click', () => {
   activeSlide--
 
   if (activeSlide < 0) {
-    activeSlide = slides.length -1
+    activeSlide = slides.length - 1
   }
 
   setBgToBody()
@@ -32,7 +32,7 @@ function setBgToBody () {
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage
 }
 
-function setActiveSlide() {
+function setActiveSlide () {
   slides.forEach(slide => {
     slide.classList.remove('active')
   })
